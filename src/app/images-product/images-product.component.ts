602045c8a77bf043f2ae88c8599efa9ed98bd577
CH19 <chcc190505@gmail.com>
2023-10-10 17:53:39 -0400

    se ha creado la clase services que hace una consulta http para obtener las imagenes de los productos

---
+++ src/app/images-product/images-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-images-product',
+  templateUrl: './images-product.component.html',
+  styleUrls: ['./images-product.component.css']
+})
+export class ImagesProductComponent {
+
+}
