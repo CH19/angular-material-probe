a4b18c244ca958240a59bd84b213bf932e9af2b8
CH19 <chcc190505@gmail.com>
2023-10-10 18:57:42 -0400

    modelos anadidos

---
+++ src/app/models/photo.ts
@@ -0,0 +1,16 @@
+// Se crea una interfaz para establecer el tipo de datos del que tienen que ser las fotos
+export interface pohtosData{
+  id: number;
+  title: string;
+  price: number;
+  category: { id: number, name: string, image: string }
+  ​​
+  creationAt: string
+  ​​
+  description: string
+  ​​
+  ​​
+  images: string[]
+  ​
+  ​​
+}
